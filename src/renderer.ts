e5cb648298d32f3e3148f9afd17ed5573e79d0e4
Olezhka-web <oleh.protsyshyn.web@gmail.com>
2024-10-31 13:15:12 +0200

    fix: updated styles

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,161 @@
+const fetchSignIn = async (email: string, password: string) => {
+  const data = await fetch(`${window.config.API_URL}/auth/sign-in`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password, isElectron: true })
+  });
+
+  const result = await data.json();
+
+  return result;
+};
+
+const fetchMe = async () => {
+  const data = await fetch(`${window.config.API_URL}/auth/me`, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const result = await data.json();
+
+  return result;
+};
+
+const fetchProjects = async () => {
+  const data = await fetch(`${window.config.API_URL}/projects`, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const result = await data.json();
+
+  return result;
+};
+
+const showLoginForm = () => {
+  const loginForm = document.getElementById('login-form');
+
+  loginForm.style.display = 'flex';
+};
+
+const hideLoginForm = () => {
+  const loginForm = document.getElementById('login-form');
+
+  loginForm.style.display = 'none';
+};
+
+const showLoggedContainer = () => {
+  const loggedContainer = document.getElementById('logged-container');
+
+  loggedContainer.style.display = 'flex';
+};
+
+const fillLoggedContainer = ({
+  first_name,
+  last_name
+}: {
+  first_name: string;
+  last_name: string;
+}) => {
+  const userElement = document.getElementById('user');
+
+  userElement.innerText = `Hello ${first_name} ${last_name}`;
+};
+
+const hideLoggedContainer = () => {
+  const loggedContainer = document.getElementById('logged-container');
+
+  loggedContainer.style.display = 'none';
+};
+
+const showProjectContainer = () => {
+  const projectContainer = document.getElementById('project-container');
+
+  projectContainer.style.display = 'flex';
+};
+
+const fillProjectContainer = (options: Array<{ id: string; name: string }>) => {
+  const projectsDropdown = document.getElementById('projects-dropdown');
+
+  options.forEach((option) => {
+    const opt = document.createElement('option');
+
+    opt.value = option.id;
+    opt.innerHTML = option.name;
+
+    projectsDropdown.appendChild(opt);
+  });
+};
+
+const hideProjectContainer = () => {
+  const projectContainer = document.getElementById('project-container');
+
+  projectContainer.style.display = 'none';
+};
+
+const showLoadingContainer = () => {
+  const loadingContainer = document.getElementById('loading-container');
+
+  loadingContainer.style.display = 'flex';
+};
+
+const hideLoadingContainer = () => {
+  const loadingContainer = document.getElementById('loading-container');
+
+  loadingContainer.style.display = 'none';
+};
+
+const afterLogin = async ({ first_name, last_name }: { first_name: string; last_name: string }) => {
+  hideLoginForm();
+
+  showLoggedContainer();
+  fillLoggedContainer({ first_name, last_name });
+
+  const projectsResponse = await fetchProjects();
+
+  if (projectsResponse.statusCode === 401) {
+    showLoginForm();
+    hideLoggedContainer();
+
+    return;
+  }
+
+  const projects = projectsResponse.data.projects;
+
+  showProjectContainer();
+  fillProjectContainer(
+    projects.map(({ id, name }: { id: string; name: string }) => ({ id, name }))
+  );
+};
+
+window.addEventListener('DOMContentLoaded', async () => {
+  hideLoginForm();
+  hideLoggedContainer();
+  hideProjectContainer();
+  hideLoadingContainer();
+
+  try {
+    const meResponse = await fetchMe();
+
+    if (meResponse.statusCode === 401) {
+      showLoginForm();
+
+      return;
+    }
+
+    const user = meResponse.data.user;
+
+    await afterLogin({ first_name: user.first_name, last_name: user.last_name });
+  } catch (error) {
+    // eslint-disable-next-line no-alert
+    alert(`Auto Login error:${(error as Error).message}`);
+  }
+});
+
 document.getElementById('login-form').addEventListener('submit', async (event) => {
   event.preventDefault();
 
@@ -5,33 +163,30 @@ document.getElementById('login-form').addEventListener('submit', async (event) =
   const password = (document.getElementById('password') as HTMLInputElement).value.trim();
 
   try {
-    const response = await fetch(`${window.config.API_URL}/auth/sign-in`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, password })
-    });
-
-    const result = await response.json();
+    const authResponse = await fetchSignIn(email, password);
 
     // eslint-disable-next-line no-alert
-    alert(result.message);
+    alert(authResponse.message);
 
-    if (response.ok) {
-      const user = result.data.user;
+    const user = authResponse.data.user;
 
-      const form = document.getElementById('login-form');
-      const loggedElement = document.getElementById('logged');
-      const userElement = document.getElementById('user');
-
-      form.style.display = 'none';
-      loggedElement.style.display = 'flex';
-
-      userElement.innerText = `Hello ${user.first_name} ${user.last_name}`;
+    if (user) {
+      await afterLogin({ first_name: user.first_name, last_name: user.last_name });
     }
   } catch (error) {
     // eslint-disable-next-line no-alert
     alert(`Login error:${(error as Error).message}`);
   }
+});
+
+document.getElementById('start-server-button').addEventListener('click', () => {
+  const projectsDropdown = document.getElementById('projects-dropdown') as HTMLSelectElement;
+
+  projectsDropdown.disabled = true;
+
+  const startServerButton = document.getElementById('start-server-button');
+
+  startServerButton.style.display = 'none';
+
+  showLoadingContainer();
 });
